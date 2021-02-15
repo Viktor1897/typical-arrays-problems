eb877e17ab0161edcd7e0ef9aaaf3f226d27c940
Viktor Kolosov <vitykolosov@gmail.com>
2021-02-15 20:27:23 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  let minElem = 0;
+  for (elem of array) {
+    if (elem<minElem) minElem = elem;
+  }
+  return minElem;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  let maxElem = 0;
+  for (elem of array) {
+    if (elem>maxElem) maxElem = elem;
+  }
+  return maxElem;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  let arrAverage = 0;
+  for (elem of array) {
+    arrAverage += elem;
+  }
+  return arrAverage/array.length || 0;
 }
